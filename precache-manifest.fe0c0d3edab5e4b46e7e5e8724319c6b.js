16d2b28f9de5f8aec8dc5a62fda99a7ded469d3f
tzvik15 <tzvik15@yahoo.com>
2020-02-16 08:44:19 -0700

    Updates

---
+++ precache-manifest.fe0c0d3edab5e4b46e7e5e8724319c6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2d6fb03197c00221a9e019365886bb",
+    "url": "/REACT-Employee-Directory/index.html"
+  },
+  {
+    "revision": "7698cf350b92d8c5e9d0",
+    "url": "/REACT-Employee-Directory/static/css/main.83d8ffdb.chunk.css"
+  },
+  {
+    "revision": "791e38e5c18bd55e59fe",
+    "url": "/REACT-Employee-Directory/static/js/2.c5875459.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/REACT-Employee-Directory/static/js/2.c5875459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7698cf350b92d8c5e9d0",
+    "url": "/REACT-Employee-Directory/static/js/main.ab1c8f76.chunk.js"
+  },
+  {
+    "revision": "95d002f5428bb4020d24",
+    "url": "/REACT-Employee-Directory/static/js/runtime-main.fa195beb.js"
+  }
+]);
